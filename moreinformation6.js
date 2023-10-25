0938bd843a63451e26f9ea428ec1e583e1cfad5f
the-dude2000 <128939099+the-dude2000@users.noreply.github.com>
2023-10-25 17:00:49 -0230

    Add files via upload

---
+++ moreinformation6.js
@@ -0,0 +1,222 @@
+/*
+
+LAVAREL
+
+Lavarel Extra Intellisense
+
+laravel blade spacer ////
+
+other things as well 
+
+composer.json holds projects assets
+
+phpstorm .
+
+cd freeCodeGram
+
+// in command prompt: ////////
+
+ laravel
+
+echo $PATH
+
+ laravel new freeCodeGram
+
+Laravel is the number 1 framework in Github
+
+go to getcomposer.org to install laravel
+
+NPM is the js counterpart to composer
+
+C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    char line[255];
+    FILE * fpointer = fopen("employees.txt", "r");
+
+    fgets(line, 255, fpointer);
+    fgets(line, 255, fpointer);
+    printf("%s", line);
+
+    fclose(fpointer);
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    FILE * fpointer = fopen("employees.txt", "a"); //"w" as well
+
+    fprintf(fpointer, "\nKelly, Customer Service");
+
+    fclose(fpointer);
+
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int age = 30;
+    int *pAge = &age;
+
+    printf("%d", *&age);
+
+    return 0;
+}
+
+
+int main(){
+
+    int age = 30;
+    int *pAge = &age;
+
+    printf("%d", *&*&age);
+
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int age = 30;
+    int *pAge = &age;
+
+    printf("%p", &age);
+
+    return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int age = 30;
+    int * pAge = &age;
+    double gpa = 3.4;
+    double * pGpa = &gpa;
+    char grade = 'A';
+    char * aGrade = &grade;
+
+
+
+
+    printf("age's memory address: %p\n", &age);
+    return 0;
+}
+
+pointer is another type of data tat's a memory address.
+to print out a pointer use %p // other things maybe too like \n
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int age = 3-;
+    double gpa = 3.4;
+    char grade = 'A';
+
+    printf("age: %p\ngpa: %p\ngrade: %p", &age, &gpa, &grade);
+
+
+
+
+   return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int nums[3][2] = {
+                        {1, 2},
+                        {3, 4},
+                        {5, 6},
+                        };
+   int i, j;
+   for(i = 0; i < 3; i++){
+    for(j = 0; j < 2; j++){
+        printf("%d", nums[i][j]);
+    }
+    printf("\n");
+   }
+
+   return 0;
+}
+
+ printf("%d", nums[1][1]);
+
+INSTRUMENTS
+
+1:44 practice! double kick speed video just do this drum beats online
+
+Control is important
+
+Exercises:
+
+First measure
+
+Quarter notes on the kick right left right left
+
+Second measure
+
+Two 16th notes
+1,1e,2,2e,3,3e,4,4e
+
+Get good muscle development
+
+exercise called the double kick subdivision ladder
+
+PIANO 
+
+DOG 
+
+| | 
+
+Get great endurance
+
+Tabata exercise
+
+do an exercise for 20 seconds as fast as you can
+
+ 10 second rest
+
+ repeat for 4 mins
+
+ Tabata
+
+ 1. Play 16th notes on double kick 20 seconds as fast
+ as you can
+
+ 2. 10 Second rest.
+
+ 3. Repeat 8x (total 4 minutes)
+
+ Pro tip
+
+ If things sound sloppy,
+ lower the speed by 10%
+
+
+*/
